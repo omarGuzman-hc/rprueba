3bf29258db536e89bd80b968df346a1887d0953c
omarGuzmn-hc <aghonducafe@gmail.com>
2020-09-05 10:49:02 -0600

    Testeing crystal

---
+++ Didactico/Didactico/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Didactico.Startup))]
+namespace Didactico
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
